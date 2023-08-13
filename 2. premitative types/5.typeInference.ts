4cb4664afbeb07784059598569b2a2df60324b42
abhithory <abhithory@gmail.com>
2023-08-14 03:48:37 +0530

    type inference

---
+++ 2. premitative types/5.typeInference.ts
@@ -0,0 +1,5 @@
+// Type inference is a feature in programming languages that allows the compiler or interpreter to automatically deduce the data type of a variable or expression without the need for explicit type annotations. In simpler terms, it's the ability of the programming language to figure out the type of a value based on how it's used in the code.
+
+
+let myFullName = "Abhishek Thory";
+let myAge = 21;
